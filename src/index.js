24e19d4b7b77f4b2a2f6e51dafb71b944e0a0992
lamtran03072000 <tranhoanglam03072000@gmail.com>
2024-01-26 20:22:09 +0700

    reactjs

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// jsx : => html được viết trong file js (nền js) được gọi là jsx
+root.render(<div>abc</div>);
